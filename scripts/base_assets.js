ccd22479ee6363ae8b1385476a4c611011e9c0f0
neverbot <ivan@neverbot.com>
2025-03-08 18:22:43 +0100

    lint fixes

--- scripts/base_assets.js
+++ scripts/base_assets.js
@@ -1,9 +1,9 @@
 const util = require('hexo-util');
 
-hexo.extend.tag.register('base_asset_img', function (args, content) {
+hexo.extend.tag.register('base_asset_img', function (args) {
   const [imageName] = args;
 
-  imgAttr = {};
+  let imgAttr = {};
   imgAttr.src = hexo.config.root + 'images/' + imageName;
 
   return util.htmlTag('img', imgAttr);
