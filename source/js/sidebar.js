fe4b438ae904889dffb29d90bd4a0b967b3e3f61
neverbot <ivan@neverbot.com>
2026-03-20 20:49:33 +0100

    refactor: rewrite sidebar.js in vanilla JS with Fisher-Yates shuffle

--- source/js/sidebar.js
+++ source/js/sidebar.js
@@ -1,30 +1,21 @@
 function sidebar() {
-  $('.sidebar ul').each(function () {
-    // get current ul
-    var $ul = $(this);
-    // get array of list items in current ul
-    var $liArr = $ul.children('li');
+  document.querySelectorAll('.sidebar ul').forEach(function (ul) {
+    var items = Array.from(ul.children);
 
-    // sort array of list items in current ul randomly
-    $liArr
-      .sort(function () {
-        return 0.5 - Math.random();
-      })
-      // append list items to ul
-      .appendTo($ul);
+    // Fisher-Yates shuffle
+    for (var i = items.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = items[i];
+      items[i] = items[j];
+      items[j] = temp;
+    }
+
+    items.forEach(function (li) {
+      ul.appendChild(li);
+    });
   });
 }
 
-// hack to wait until the DOM is really loaded
-
-// Helper function
-// const domReady = (cb) => {
-//   document.readyState === 'interactive' || document.readyState === 'complete'
-//     ? cb()
-//     : document.addEventListener('DOMContentLoaded', cb);
-// };
-
-$(window).on('load', function () {
-  // domReady(galleries);
+window.addEventListener('load', function () {
   sidebar();
 });
